6a8cac466018ced6f0649e7c4bc12e1fc3b7860a
takedataisei <tsukuda120835@gmail.com>
2024-05-16 21:37:44 +0900

    新規登録機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
+                                 message: 'is invalid. Include both letters and numbers' }
+
+  with_options presence: true do
+    validates :nickname
+    validates :family_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
+                                      message: 'is invalid. Input full-width characters.' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/,
+                                     message: 'is invalid. Input full-width characters.' }
+    validates :family_name_kana, format: { with: /\A[ァ-ヶー]+\z/,
+                                           message: 'is invalid. Input full-width katakana characters.' }
+    validates :first_name_kana, format: { with: /\A[ァ-ヶー]+\z/,
+                                          message: 'is invalid. Input full-width katakana characters.' }
+    validates :birth_date
+  end
 end
