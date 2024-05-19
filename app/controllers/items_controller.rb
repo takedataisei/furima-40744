5ed11b56b7b263aa2e6d24fb31d489a12531a3e4
takedataisei <tsukuda120835@gmail.com>
2024-05-19 18:51:46 +0900

    set_itemの実装と、update五のリダイレクトの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
   before_action :move_to_root, only: [:edit]
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.order(created_at: :DESC)
@@ -20,17 +21,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to
+      redirect_to item_path(@item.id)
     else
       render :edit, status: :unprocessable_entity
     end
@@ -45,5 +43,9 @@ class ItemsController < ApplicationController
 
   def move_to_root
     redirect_to root_path if Item.find(params[:id]).user_id != current_user.id
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
