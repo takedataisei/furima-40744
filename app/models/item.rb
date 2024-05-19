66d304ca3b21992e14cdbabae6cb346a2e9e76d2
takedataisei <tsukuda120835@gmail.com>
2024-05-19 14:11:44 +0900

    実装案内コメントの削除とpriceカラムのバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,7 +11,8 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   validates :name, :item_detail, :image, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, 
+                                                    less_than_or_equal_to: 9_999_999 }
 
   with_options numericality: { other_than: 1, message: "can't be blank" } do
     validates :genre_id
