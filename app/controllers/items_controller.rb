d201c522ca237b69212d2d2a335d2b6c8246aa7f
takedataisei <tsukuda120835@gmail.com>
2024-05-19 18:25:18 +0900

    商品情報編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
+  before_action :move_to_root, only: [:edit]
+
   def index
     @items = Item.order(created_at: :DESC)
   end
@@ -21,10 +23,27 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
     params.require(:item).permit(:name, :item_detail, :price, :genre_id, :item_condition_id, :image,
                                  :shipping_cost_burden_id, :prefecture_id, :shipping_day_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_root
+    redirect_to root_path if Item.find(params[:id]).user_id != current_user.id
   end
 end
