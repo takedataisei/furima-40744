a04357beb307e86a2ebbc835e1bffcb652de64cd
takedataisei <tsukuda120835@gmail.com>
2024-05-22 15:30:28 +0900

    販売状態、ログイン状態で、表示、遷移するページの切り替え

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item
+  before_action :check_item_owner
+  before_action :check_item_sold
+
   def index
     gon.public_key = ENV['PAYJP_PUBLIC_KEY']
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
@@ -25,8 +29,19 @@ class OrdersController < ApplicationController
                                           :building_name).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
   end
 
-  def pay_item
+  def set_item
     @item = Item.find(params[:item_id])
+  end
+
+  def check_item_owner
+    redirect_to root_path if @item.user_id == current_user.id
+  end
+
+  def check_item_sold
+    redirect_to root_path if Order.exists?(item_id: @item.id)
+  end
+
+  def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
